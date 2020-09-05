6045007d1b2420d580f94794d2a860284d65d3bc
edijun <edijun.design@gmail.com>
2020-09-05 17:18:51 +0700

    initial commit
    initial commit

---
+++ src/main/java/id/edijun/springbootbaseservice/SpringBootBaseServiceApplication.java
@@ -0,0 +1,13 @@
+package id.edijun.springbootbaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBaseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/edijun/springbootbaseservice/SpringBootBaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.edijun.springbootbaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
